0a4781095399c8c14831500bdb830de613c1eb36
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-01-25 02:56:35 +0530

    layouts/Default/index: Add more weights

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -12,7 +12,7 @@ const DefaultLayout: React.FC = (props) => {
 	return (
 		<>
 			<GoogleFonts href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" />
-			<GoogleFonts href="https://fonts.googleapis.com/css?family=Montserrat:400,700" />
+			<GoogleFonts href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,500&display=swap" />
 
 			<Head>
 				<link
