68acd6d4e0624211ffa68f94db35ffa474186b27
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-01-26 12:23:54 +0530

    components/Banner/styles: Change Layout back to end

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -37,6 +37,7 @@ export const KeepGoingButton = styled.a`
   align-items: flex-end;
   cursor:pointer;
   z-index:6;
+  margin-bottom:2vh;
 
   &:hover{
     opacity:0.5;
@@ -97,7 +98,7 @@ padding-bottom:5vh;
 `
 
 export const FrontPageContainer = styled.div`
-
+height:100vh;
 display: flex;
 flex-direction: column;
 align-items: center;
