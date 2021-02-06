f780dd57cb44ed22a26e8cc83027595f73a83b79
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-06 20:39:59 +0530

    pages/repoCount: Make code more readable

--- src/pages/repoCount.tsx
+++ src/pages/repoCount.tsx
@@ -4,27 +4,24 @@ import React, { useEffect } from "react";
 import { Container } from "@/views/Home/ProjectsSection/PortfolioItems/styles";
 
 
-async function fetcher(...arg){
-	const res = await fetch(...arg);
 
-	return res.json();
-}
+const fetcher = url => fetch(url).then(r => r.json())
 
 const repoCount: NextPage = () => {
-	const { data } = useSwr("/api/github", fetcher);
+	const { data , error} = useSwr("/api/github", fetcher);
 
-	useEffect(() => {
-		console.log(data);
-	})
+	
 
+	if (error) return <div>failed to load</div>
+	if (!data) return <h1>Loading...</h1>	
 
 	return(
 		<div>
-			{
-				data 
-					? <h1>{+data.count.data.owned_private_repos + +data.count.data.public_repos}</h1>
-					: "Loading"
-			}
+			
+
+			<h1>{+data.count.data.owned_private_repos + +data.count.data.public_repos}</h1>
+					
+		
 
 
 		</div>
