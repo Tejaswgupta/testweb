cd4fdf3405402d2bdc16ba4ab6667122b76a95eb
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-06 19:56:42 +0530

    pages/[slug].test: refactor old slug

---
+++ src/pages/[slug].test.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { GetStaticProps, GetStaticPaths, NextPage } from "next"
+
+import {
+	getAllDetailedPosts,
+	getDetailedPostBySlug,
+	DetailedPost
+} from "@/lib/posts"
+
+import BlogPostLayout from "@/layouts/BlogPost"
+
+import PostSingleSection from "@/views/Projects/PostSingle"
+
+type PostProps = {
+	post: DetailedPost
+}
+
+const Post: NextPage<PostProps> = (props) => {
+	const { post } = props
+
+	return (
+		<BlogPostLayout
+			title={post.title}
+			description={post.description}
+			coverSrc={post.coverSrc}
+			slug={post.slug}
+			authorName={post.authorName}
+		>
+			<PostSingleSection
+				post={post}
+			/>
+		</BlogPostLayout>
+	)
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const posts = await getAllDetailedPosts()
+
+	const paths = posts.map(post => ({ params: { slug: post.slug } }))
+
+	return {
+		paths: paths,
+		fallback: false
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const post = await getDetailedPostBySlug(params.slug as string)
+
+	return {
+		props: {
+			post
+		}
+	}
+}
+
+export default Post
