d5153982578049fe29e2a42d3b84894857e9227c
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-06 19:58:37 +0530

    pages/api/github: create API to fetch data from Github

---
+++ src/pages/api/github.ts
@@ -0,0 +1,28 @@
+const {Octokit} = require("@octokit/rest");
+
+export default async (req,res)=>{
+
+
+
+	const octokit = new Octokit({
+		auth: "997229a26c0d7f2cf42280c80dad8bd84f7c4987"
+	})
+
+	const repos = await octokit.repos.listForAuthenticatedUser(
+		{
+			visibility:"all",
+			per_page: "100"
+		}
+	);
+    
+	const count = await octokit.request("GET /user")
+
+	console.log(count);
+
+
+	return res.status(200).json({
+		repo: repos,
+		count: count,
+	});
+
+}
