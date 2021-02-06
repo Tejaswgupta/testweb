6b4b2912a6c45deee4f04e72a21e53f950522c0e
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-06 20:58:11 +0530

    Improve api code

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -8,20 +8,20 @@ export default async (req,res)=>{
 		auth: "997229a26c0d7f2cf42280c80dad8bd84f7c4987"
 	})
 
-	const repos = await octokit.repos.listForAuthenticatedUser(
-		{
-			visibility:"all",
-			per_page: "100"
-		}
-	);
+	// const repos = await octokit.repos.listForAuthenticatedUser(
+	// 	{
+	// 		visibility:"all",
+	// 		per_page: "100"
+	// 	}
+	// );
     
-	const count = await octokit.request("GET /user")
+	const count = await octokit.request("GET /user").catch((e)=>{console.log(e)});
 
 	console.log(count);
 
 
 	return res.status(200).json({
-		repo: repos,
+		// repo: repos,
 		count: count,
 	});
 
