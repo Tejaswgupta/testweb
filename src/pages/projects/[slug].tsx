74d7666794b0e147a1ab0911d93035153fb0e00f
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-07 13:22:16 +0530

    Use dumb code to check error

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,55 +1,68 @@
-import React, { useEffect } from "react"
-import { NextPage } from "next"
+// import React, { useEffect } from "react"
+// import { NextPage } from "next"
 
-import {
-	DetailedPost,
-} from "@/lib/posts"
+// import {
+// 	DetailedPost,
+// } from "@/lib/posts"
 
-import BlogPostLayout from "@/layouts/BlogPost"
+// import BlogPostLayout from "@/layouts/BlogPost"
 
-import PostSingleSection from "@/views/Projects/PostSingle"
-import { useRouter } from "next/dist/client/router"
+// import PostSingleSection from "@/views/Projects/PostSingle"
+// import { useRouter } from "next/dist/client/router"
 
-type PostProps = {
-	post: DetailedPost
+// type PostProps = {
+// 	post: DetailedPost
+// }
+
+// const Post: NextPage<PostProps> = () => {
+
+// 	const router = useRouter();
+// 	const { project_name, project_description , project_image , tags } = router.query;
+// 	// const x = tags ?? ["Misc"];
+
+// 	useEffect(() => {
+// 		console.log(tags);
+// 	});
+
+// 	const customProp: DetailedPost = {
+// 		title: project_name as string,  
+// 		description: project_description as string, 
+// 		coverSrc: project_image as string,
+// 		slug:"{post.slug}",
+// 		authorName:"Tejaswa Gupta",
+// 		tags: [],
+// 		authorAvatarSrc: "/images/new-logo.webp",
+// 		content: "",
+// 		date:"date"
+// 	}; 
+
+// 	return (
+// 		<BlogPostLayout
+// 			title= {project_name as string} 
+// 			description={project_description as string}
+// 			coverSrc={project_image as string}
+// 			slug="{post.slug}"
+// 			authorName='Tejaswa Gupta'
+// 		>
+// 			<PostSingleSection
+// 				{...customProp}
+// 			/>
+// 		</BlogPostLayout>
+// 	)
+// }
+
+
+// export default Post
+
+
+import { useRouter } from "next/router"
+import React from "react"
+
+const Post = () => {
+	const router = useRouter()
+	const { pid } = router.query
+
+	return <p>Post: {pid}</p>
 }
-
-const Post: NextPage<PostProps> = () => {
-
-	const router = useRouter();
-	const { project_name, project_description , project_image , tags } = router.query;
-	// const x = tags ?? ["Misc"];
-
-	useEffect(() => {
-		console.log(tags);
-	});
-
-	const customProp: DetailedPost = {
-		title: project_name as string,  
-		description: project_description as string, 
-		coverSrc: project_image as string,
-		slug:"{post.slug}",
-		authorName:"Tejaswa Gupta",
-		tags: [],
-		authorAvatarSrc: "/images/new-logo.webp",
-		content: "",
-		date:"date"
-	}; 
-
-	return (
-		<BlogPostLayout
-			title= {project_name as string} 
-			description={project_description as string}
-			coverSrc={project_image as string}
-			slug="{post.slug}"
-			authorName='Tejaswa Gupta'
-		>
-			<PostSingleSection
-				{...customProp}
-			/>
-		</BlogPostLayout>
-	)
-}
-
 
 export default Post
