fe47820016f97e6e4f3030e82c01596059d2643e
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-02-07 10:58:26 +0530

    pages/api/github: Wrap code in try catch and provide explicit typees

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -1,28 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 const {Octokit} = require("@octokit/rest");
 
-export default async (req,res)=>{
+export default async (req: NextApiRequest,res:NextApiResponse) => { 
 
 
+	try{
 
-	const octokit = new Octokit({
-		auth: "997229a26c0d7f2cf42280c80dad8bd84f7c4987"
-	})
-
-	// const repos = await octokit.repos.listForAuthenticatedUser(
-	// 	{
-	// 		visibility:"all",
-	// 		per_page: "100"
-	// 	}
-	// );
+		const octokit = new Octokit({
+			auth: "997229a26c0d7f2cf42280c80dad8bd84f7c4987"
+		})
+		const count = await octokit.request("GET /user");
+		// const repos = await octokit.repos.listForAuthenticatedUser(
+		// 	{
+		// 		visibility:"all",
+		// 		per_page: "100"
+		// 	}
+		// );
     
-	const count = await octokit.request("GET /user").catch((e)=>{console.log(e)});
+		return res.status(200).json({
+			// repo: repos,
+			count: count,
+		});
+	
+	}
+	catch(error){
+		console.log(error);
+	}
 
-	console.log(count);
 
 
-	return res.status(200).json({
-		// repo: repos,
-		count: count,
-	});
+	
+	
+	
 
+	
+
+
+	
 }
